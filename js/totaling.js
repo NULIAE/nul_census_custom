bddb6aa70617f2d1fd9a17e1b0a4a0ebe53dc0d5
Alice Frumin <alice@aghstrategies.com>
2017-10-16 15:57:59 -0400

    totaling

--- js/totaling.js
+++ js/totaling.js
@@ -1,3 +1,33 @@
 (function ($) {
-console.log('linked');
+  var tableFields = [
+    'input#edit-field-voter-white-male-und-0-value',
+    'input#edit-field-voter-white-female-und-0-value',
+    'input#edit-field-voter-hispanic-male-und-0-value',
+    'input#edit-field-voter-hispanic-female-und-0-value',
+    'input#edit-field-voter-asian-amer-male-und-0-value',
+    'input#edit-field-voter-asian-amer-female-und-0-value',
+    'input#edit-field-voter-native-amer-male-und-0-value',
+    'input#edit-field-voter-native-amer-female-und-0-value',
+    'input#edit-field-voter-african-amer-male-und-0-value',
+    'input#edit-field-voter-african-amer-female-und-0-value',
+    'input#edit-field-voter-other-male-und-0-value',
+    'input#edit-field-voter-other-female-und-0-value',
+  ];
+  $(document).ready(function () {
+    // $('input#edit-field-voter-societal-impact-und-0-value').attr('disabled', true);
+    var addFields = function () {
+      var total = parseInt(0);
+      $(tableFields).each(function (index, field) {
+        if (parseInt($(field).val())) {
+          total = parseInt(total) + parseInt($(field).val());
+        }
+      });
+
+      $('input#edit-field-voter-societal-impact-und-0-value').val(total);
+    };
+
+    $(tableFields).each(function (index, field) {
+      $(field).keyup(addFields);
+    });
+  });
 }(jQuery));
