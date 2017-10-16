fc989311d2713687da97e64f5804d074f466e544
Alice Frumin <alice@aghstrategies.com>
2017-10-16 17:44:35 -0400

    AGH #10848 totalling for board

--- js/totaling.js
+++ js/totaling.js
@@ -1,6 +1,5 @@
 (function ($) {
-
-  // TODO add all table bases and total field Ids to this array
+  // TODO add all table bases and total field Ids to this array for tables with one total
   var tableFieldBases = [
      {
       base: 'field-voter-',
@@ -10,19 +9,46 @@
       base: 'field-advocacy-',
       totalFieldId: 'edit-field-advocacy-societal-impact-und-0-value',
     },
+    {
+      base: 'field-board-',
+      totalFieldId: 'edit-field-board-member-grand-total-und-0-value',
+    },
   ];
-  var races = ['white', 'hispanic', 'asian', 'native', 'african', 'other'];
+  var races = ['white', 'hispanic', 'asian', 'native', 'african-amer', 'other'];
   $(document).ready(function () {
     $(tableFieldBases).each(function (index, fieldBase) {
       $('input[id*="' + fieldBase.base + '"').keyup(function () {
         var total = parseInt(0);
         $(races).each(function (index, race) {
           $('input[id*="' + fieldBase.base + race + '"').each(function (index, field) {
+            // grand total
             if (this.id !== fieldBase.totalFieldId) {
-              if (parseInt($(field).val())) {
-                total = parseInt(total) + parseInt($(field).val());
+              if (this.id.indexOf('total') < 0) {
+                if (parseInt($(field).val())) {
+                  total = parseInt(total) + parseInt($(field).val());
+                }
+              }
+
+              // Total by race (for board)
+              if (this.id.indexOf('total') > 0) {
+                var totByRace = parseInt(0);
+
+                if (parseInt($('input[id*="' + fieldBase.base + race + '-male"').val())) {
+                  var totByRace = totByRace + parseInt(
+                    $('input[id*="' + fieldBase.base + race + '-male"').val()
+                  );
+                }
+
+                if (parseInt($('input[id*="' + fieldBase.base + race + '-female"').val())) {
+                  var totByRace = totByRace + parseInt(
+                    $('input[id*="' + fieldBase.base + race + '-female"').val()
+                  );
+                }
+
+                $(this).val(totByRace);
               }
             }
+
           });
 
           $('input#' + fieldBase.totalFieldId).val(total);
