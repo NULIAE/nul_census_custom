f63a3e5916e30a731e9a34d92c81e5b119857300
Alice Frumin <alice@aghstrategies.com>
2017-10-16 16:45:19 -0400

    AGH #10848 basic totaling working

--- js/totaling.js
+++ js/totaling.js
@@ -1,33 +1,33 @@
 (function ($) {
-  var tableFields = [
-    'input#edit-field-voter-white-male-und-0-value',
-    'input#edit-field-voter-white-female-und-0-value',
-    'input#edit-field-voter-hispanic-male-und-0-value',
-    'input#edit-field-voter-hispanic-female-und-0-value',
-    'input#edit-field-voter-asian-amer-male-und-0-value',
-    'input#edit-field-voter-asian-amer-female-und-0-value',
-    'input#edit-field-voter-native-amer-male-und-0-value',
-    'input#edit-field-voter-native-amer-female-und-0-value',
-    'input#edit-field-voter-african-amer-male-und-0-value',
-    'input#edit-field-voter-african-amer-female-und-0-value',
-    'input#edit-field-voter-other-male-und-0-value',
-    'input#edit-field-voter-other-female-und-0-value',
+
+  // TODO add all table bases and total field Ids to this array
+  var tableFieldBases = [
+     {
+      base: 'field-voter-',
+      totalFieldId: 'edit-field-voter-societal-impact-und-0-value',
+    },
+    {
+      base: 'field-advocacy-',
+      totalFieldId: 'edit-field-advocacy-societal-impact-und-0-value',
+    },
   ];
+  var races = ['white', 'hispanic', 'asian', 'native', 'african', 'other'];
   $(document).ready(function () {
-    // $('input#edit-field-voter-societal-impact-und-0-value').attr('disabled', true);
-    var addFields = function () {
-      var total = parseInt(0);
-      $(tableFields).each(function (index, field) {
-        if (parseInt($(field).val())) {
-          total = parseInt(total) + parseInt($(field).val());
-        }
+    $(tableFieldBases).each(function (index, fieldBase) {
+      $('input[id*="' + fieldBase.base + '"').keyup(function () {
+        var total = parseInt(0);
+        $(races).each(function (index, race) {
+          $('input[id*="' + fieldBase.base + race + '"').each(function (index, field) {
+            if (this.id !== fieldBase.totalFieldId) {
+              if (parseInt($(field).val())) {
+                total = parseInt(total) + parseInt($(field).val());
+              }
+            }
+          });
+
+          $('input#' + fieldBase.totalFieldId).val(total);
+        });
       });
-
-      $('input#edit-field-voter-societal-impact-und-0-value').val(total);
-    };
-
-    $(tableFields).each(function (index, field) {
-      $(field).keyup(addFields);
     });
   });
 }(jQuery));
