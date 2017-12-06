f99ad553e72ef9c55bfa3991138523d8cdc7bd96
Alice Frumin <alice@aghstrategies.com>
2017-12-06 18:48:40 -0500

    working on workflow from reviewed to reveiewed complete

--- js/addButtons.js
+++ js/addButtons.js
@@ -1,10 +1,20 @@
 (function ($) {
   $(document).ready(function () {
     var href = window.location.pathname;
+    if (Drupal.settings.nul_census_custom.censuspart == 'tab') {
+      var buttonsToAdd = "<li><a href='"
+       + href  +
+       "/Reviewed'>Mark Reviewed</a></li><li><a href='"
+       + href  + "/Resubmit'>Mark Resubmit</a></li>";
+    }
+
+    if (Drupal.settings.nul_census_custom.censuspart == 'census') {
+      var buttonsToAdd = "<li><a href='"
+       + href  +
+       "/ReviewComplete'>Mark Census Review Complete</a></li";
+    }
 
     // Insert buttons to mark as reviewed and resubmit
-    $($('ul.tabs.primary')).prepend(
-      "<li><a href='" + href  + "/Reviewed'>Mark Reviewed</a></li><li><a href='" + href  + "/Resubmit'>Mark Resubmit</a></li>"
-    );
+    $($('ul.tabs.primary')).prepend(buttonsToAdd);
   });
 }(jQuery));
