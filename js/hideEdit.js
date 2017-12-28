f025e725dd8073eeefb9fd57be5027c4b057f1e3
Alice Frumin <alice@aghstrategies.com>
2017-12-28 14:44:07 -0500

    removing hide edit js... doing all permissioning thru grants hook

--- js/hideEdit.js
+++
@@ -1,9 +0,0 @@
-(function ($) {
-  $(document).ready(function () {
-    // TODO this may not be necessary now with new permissions
-    // hide edit link on view of content tab if do not have permission
-    $('a[href*="edit"]').each(function () {
-      $(this).hide();
-    });
-  });
-}(jQuery));
