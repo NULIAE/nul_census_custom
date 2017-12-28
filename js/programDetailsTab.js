d37b3e835a3d98218b0e7ec01c9e49246e1a97c2
Alice Frumin <alice@aghstrategies.com>
2017-12-28 15:47:23 -0500

    AGH #11460 updating js for program details tabs after updating field to be a boolean

--- js/programDetailsTab.js
+++ js/programDetailsTab.js
@@ -3,10 +3,10 @@
   // the user clicks "Do you offer Programs of this type?"
   $(document).ready(function () {
     hideFields();
-    $('input#edit-field-do-you-offer-programs-of-t-und').change(hideFields);
-
+    $('input[type=radio][name^=field_do_you_offer_programs_of_t]').change(hideFields);
     function hideFields() {
-      if ($('input#edit-field-do-you-offer-programs-of-t-und').attr('checked') == true) {
+      console.log($('input[type=radio][name^=field_do_you_offer_programs_of_t]:checked').val());
+      if ($('input[type=radio][name^=field_do_you_offer_programs_of_t]:checked').val() == 1) {
         $('.node-form div.form-item').each(function (index, key) {
           // skips the title, tab status and parent census fields,
           // whether the user sees those fields should be depenedent on their permissions
@@ -23,7 +23,7 @@
         });
       }
 
-      if ($('input#edit-field-do-you-offer-programs-of-t-und').attr('checked') == false) {
+      if ($('input[type=radio][name^=field_do_you_offer_programs_of_t]:checked').val() == 0) {
         $('.node-form div.form-item').each(function (index, key) {
           // skips the title, tab status and parent census fields,
           // whether the user sees those fields should be depenedent on their permissions
