40555006054f0bf7acdc7d44dcbc8c18038c3350
Alice Frumin <alice@aghstrategies.com>
2018-02-01 17:38:18 -0500

    loading js to hide services provided options now that they are on a paragraph

---
+++ js/servicesProvided.js
@@ -0,0 +1,6 @@
+(function ($) {
+  // For some reason table on emergency_relief_activities needs extra js
+  $(document).ready(function () {
+    console.log($('.field-type-taxonomy-term-reference.field-name-field-program-service-provided'));
+  });
+}(jQuery));
