efcec82e7e71383042cfefe679ba73d70d1fa64a
Alice Frumin <alice@aghstrategies.com>
2018-02-06 15:15:45 -0500

    sorting first services provided paragraph

--- js/servicesProvided.js
+++ js/servicesProvided.js
@@ -1,6 +1,29 @@
 (function ($) {
   // For some reason table on emergency_relief_activities needs extra js
   $(document).ready(function () {
-    console.log($('.field-type-taxonomy-term-reference.field-name-field-program-service-provided'));
+    // options sorted by tid of program area set from php
+    var sortedOptions = Drupal.settings.nul_census_custom.sorted;
+
+    // if Program Area is set on page load get Services For that program area and save to variable
+    if ($('#edit-field-program-area-und').val() > 0) {
+      var programArea = $('#edit-field-program-area-und').val();
+      var servicesForArea = sortedOptions[programArea];
+    }
+
+    // TODO what should happen if an admin user changes the program area..
+    // should services provided paragraphs all be reset?
+    // $('#edit-field-program-area-und').change();
+
+    // When a new service provided paragraph is added
+    $(document).ajaxStop(function () {
+      // foreach thru options if now in services for Area array unset
+      if ($('select[name^="field_services_provided"]').length > 0) {
+        $('select[name^="field_services_provided"] option').each(function (id, value) {
+          if (!servicesForArea[id]) {
+            $(this).remove();
+          }
+        });
+      }
+    });
   });
 }(jQuery));
