45ee6d4403909ec03730938678512e0dfaef4bd7
Alice Frumin <alice@aghstrategies.com>
2018-02-08 11:01:52 -0500

    AGH 11789 Js to calculate revenue totals

--- js/revenueTotal.js
+++ js/revenueTotal.js
@@ -1,15 +1,26 @@
 (function ($) {
-  // THIS JS handles tables with total fields that need to be calcualted on the revenue page
-  //TODO add revenue field names
-  var fieldsToTotal = [];
-  var totalField = '';
+  // AGH 11789 THIS JS handles tables with total fields that need to be calcualted on the revenue page
   $(document).ready(function () {
+    var fieldsToTotal = [
+      'input#edit-field-revenue-investment-und-0-value',
+      'input#edit-field-revenue-corporations-und-0-value',
+      'input#edit-field-revenue-foundations-und-0-value',
+      'input#edit-field-revenue-individual-members-und-0-value',
+      'input#edit-field-revenue-special-events-und-0-value',
+      'input#edit-field-revenue-united-way-und-0-value',
+      'input#edit-field-revenue-federal-und-0-value',
+      'input#edit-field-revenue-state-local-und-0-value',
+      'input#edit-field-revenue-nul-und-0-value',
+    ];
+    var totalField = 'input#edit-field-revenue-total-budget-und-0-value';
+
     // Disable total fields so user cannot enter content
     $(totalField).attr('readonly', true);
     $(totalField).addClass('readonly');
 
     // Function to calculate fields
-    var calculateTotal = function (fieldsToTotal, totalField) {
+    var calculateTotalRevenue = function () {
+
       // set total to 0
       var total = parseInt(0);
       $(fieldsToTotal).each(function (i, field) {
@@ -18,15 +29,15 @@
         }
       });
 
-      $('input#' + totalField).val(total);
+      $(totalField).val(total);
     };
 
     // calculate total on page load
-    calculateTotal(fieldsToTotal, totalField);
+    calculateTotalRevenue(fieldsToTotal, totalField);
 
     // on key up recalculate total
     $(fieldsToTotal).each(function (index, fieldName) {
-      $('input[id*="' + fieldName + '"').keyup(calculateTotal(fieldsToTotal, totalField));
+      $(fieldName).keyup(calculateTotalRevenue);
     });
   });
 }(jQuery));
