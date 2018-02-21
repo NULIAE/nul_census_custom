f5d0cdb5e869960b82b90d82736149852785f5ee
Alice Frumin <alice@aghstrategies.com>
2018-02-21 11:34:03 -0500

    AGH #11495 adding paragraphs on  revenue and entrep tabs

--- js/paragraphTables.js
+++ js/paragraphTables.js
@@ -5,21 +5,27 @@
 
     var concatonateTables = function (tableClass) {
       var table;
-      $('div.nulTable' + tableClass).each(function (index, value) {
-        // This is the first row and will be used for the base table
-        if (index == 0) {
-          table = value;
-        }
+      if ($('div.nulTable' + tableClass).length > 0) {
+        $('div.nulTable' + tableClass).each(function (index, value) {
+          // This is the first row and will be used for the base table
+          if (index == 0) {
+            table = value;
+          }
 
-        if (index > 0) {
-          console.log($(value).find('div.nulTableRow'));
-          $(table).append($(value).find('div.nulTableRow'));
-          $(value).hide();
-        }
-      });
+          if (index > 0) {
+            $(table).append($(value).find('div.nulTableRow'));
+            $(value).hide();
+          }
+        });
+      }
     };
 
-    var paragraphGroups = ['.group-funding-table', '.group-program-service-table'];
+    var paragraphGroups = [
+      '.group-funding-table',
+      '.group-program-service-table',
+      '.group-venture-table',
+      '.group-business-table',
+    ];
     $(paragraphGroups).each(function (index, className) {
       var updatedTable = concatonateTables(className);
     });
