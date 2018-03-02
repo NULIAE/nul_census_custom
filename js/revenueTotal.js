303580673c953b0b2fae7b20bbcd1c2e7d74e495
Alice Frumin <alice@aghstrategies.com>
2018-03-02 16:37:09 -0500

    AGH #12006 add venture income fields into other field

--- js/revenueTotal.js
+++ js/revenueTotal.js
@@ -12,7 +12,7 @@
       'input#edit-field-revenue-federal-und-0-value',
       'input#edit-field-revenue-state-local-und-0-value',
       'input#edit-field-revenue-nul-und-0-value',
-      'input#edit-field-revenue-other-und-0-value', 
+      'input#edit-field-revenue-other-und-0-value',
     ];
     var totalField = 'input#edit-field-revenue-total-budget-und-0-value';
 
@@ -20,11 +20,27 @@
     $(totalField).attr('readonly', true);
     $(totalField).addClass('readonly');
 
+    // Disable other field which is a subtotal of the venture type fields
+    $('input#edit-field-revenue-other-und-0-value').attr('readonly', true);
+    $('input#edit-field-revenue-other-und-0-value').addClass('readonly');
+
     // Function to calculate fields
     var calculateTotalRevenue = function () {
 
+      // add venture type fields together and save to other field
+      var ventureTypeTotal = parseInt(0);
+      $('input[id*="field-budget-venture-earned-und"]').each(function (i, field) {
+        if (parseInt($(field).val())) {
+          ventureTypeTotal = parseInt(ventureTypeTotal) + parseInt($(field).val());
+        }
+      });
+
+      $('input#edit-field-revenue-other-und-0-value').val(ventureTypeTotal);
+
       // set total to 0
       var total = parseInt(0);
+
+      // Add normal fields
       $(fieldsToTotal).each(function (i, field) {
         if (parseInt($(field).val())) {
           total = parseInt(total) + parseInt($(field).val());
@@ -37,9 +53,19 @@
     // calculate total on page load
     calculateTotalRevenue(fieldsToTotal, totalField);
 
-    // on key up recalculate total
+    // on key up for normal fields recalculate total
     $(fieldsToTotal).each(function (index, fieldName) {
       $(fieldName).keyup(calculateTotalRevenue);
+    });
+
+    // on key up for amount earned field in venture type paragraph recalculate total
+    // (for existing paragraphs)
+    $('input[id*="field-budget-venture-earned-und"]').keyup(calculateTotalRevenue);
+
+    // when a new paragraph is added
+    $(document).ajaxStop(function () {
+      // on key up for amount earned field in venture type paragraph recalculate total
+      $('input[id*="field-budget-venture-earned-und"]').keyup(calculateTotalRevenue);
     });
   });
 }(jQuery));
